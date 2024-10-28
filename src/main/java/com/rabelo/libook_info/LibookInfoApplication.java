11aefe9cb2bb33a8c8788988903eb3c12e42c43c
kamille-rabelo <kamillersalgado@gmail.com>
2024-10-28 15:41:07 -0300

    Initial commit

---
+++ src/main/java/com/rabelo/libook_info/LibookInfoApplication.java
@@ -0,0 +1,13 @@
+package com.rabelo.libook_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibookInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibookInfoApplication.class, args);
+	}
+
+}
